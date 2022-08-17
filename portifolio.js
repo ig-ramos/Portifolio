37ff00508fe28a73fc1554afad923aa155a114da
ig-ramos <igorroliveira28@gmail.com>
2022-08-17 13:46:17 -0300

    Adição do JavaScript

---
+++ portifolio.js
@@ -0,0 +1,14 @@
+'use strict'
+const switcher = document.querySelector('.btn');
+switcher.addEventListener('click', function(){
+    document.body.classList.toggle('dark-theme')
+
+        var className = document.body.className;
+        if(className == 'ligth-theme'){
+            this.textContent = "Dark"
+        }else{
+            this.textContent = "Ligth"
+        }
+
+        console.log('Esta funcionando perfeitamente: ' + className);
+});
